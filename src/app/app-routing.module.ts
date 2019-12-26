7ef6668447c1588ee06a5718c354da70dad6ae0b
amaurydelassus <amaury.delassus@gmail.com>
2019-12-26 13:32:19 +0100

    #1 APP

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
+import { DashboardComponent }   from './component/dashboard/dashboard.component';
+import { HeroesComponent }      from './component/heroes/heroes.component';
+import { HeroDetailComponent }  from './component/hero-detail/hero-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:_id', component: HeroDetailComponent },
+  { path: 'heroes', component: HeroesComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+import { AppRoutingModule }     from './app-routing.module';
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './component/dashboard/dashboard.component';
+import { HeroDetailComponent }  from './component/hero-detail/hero-detail.component';
+import { HeroesComponent }      from './component/heroes/heroes.component';
+import { HeroSearchComponent }  from './component/hero-search/hero-search.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    HeroSearchComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
