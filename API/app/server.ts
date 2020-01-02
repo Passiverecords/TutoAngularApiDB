437cd389b30fc047865ab3427ff37bcbc69168b1
amaurydelassus <amaury.delassus@gmail.com>
2020-01-02 16:03:52 +0100

    HEROKU START

--- API/app/server.ts
+++ API/app/server.ts
@@ -3,7 +3,7 @@ import * as mongoose from "mongoose";
 const PORT = process.env.PORT || 3000;
 
 var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
-var urlmongo = "YOUR DB URL";
+var urlmongo = "YourApiAddress";
 mongoose.connect(urlmongo, options);
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
